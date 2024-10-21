d21a237b78be20224da5b3369041738d89499cdd
TinkerGaming87 <117561252+TinkerGaming87@users.noreply.github.com>
2024-10-21 18:22:40 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+version '2.1.2'
+description 'Clean and fully configurable loading screen for FiveM'
+author 'David Malchin <malchin459@gmail.com>'
+repository 'https://github.com/D4isDAVID/loadscreen'
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+client_scripts {
+    'client/shutdown.lua',
+    'html/assests/videos/*'
+}
+
+server_scripts {
+    'server/handover.js',
+    
+}
+
+files {
+    'html/**',
+}
+
+loadscreen 'html/index.html'
+loadscreen_cursor 'yes'
+loadscreen_manual_shutdown 'yes'
